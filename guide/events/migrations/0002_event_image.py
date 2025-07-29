1f8731f73e0484e346582156319e4ad65d645cff
Christopher Gantes <78065304+christophergantes@users.noreply.github.com>
2025-07-28 20:21:21 -0400

    Added venue functionality. Admin can create venues. Users can select a venue and it will display venue on post.

--- guide/events/migrations/0002_event_image.py
+++ guide/events/migrations/0002_event_image.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('events', '0001_initial'),
+        ("events", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='event',
-            name='image',
-            field=models.ImageField(blank=True, upload_to='event_images'),
+            model_name="event",
+            name="image",
+            field=models.ImageField(blank=True, upload_to="event_images"),
         ),
     ]
--- guide/events/models.py
+++ guide/events/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Venue(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=127)
+    state = models.CharField(max_length=127)
+    postal_code = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f"{self.name}, {self.address}, {self.city}"
+
+
 class Event(models.Model):
     title = models.CharField(max_length=127)
     description = models.TextField(max_length=511)
@@ -10,6 +21,11 @@ class Event(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     image = models.ImageField(upload_to="event_images", blank=True)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, blank=True, null=True)
+    custom_address = models.CharField(max_length=255, blank=True, null=True)
+    custom_city = models.CharField(max_length=127, blank=True, null=True)
+    custom_state = models.CharField(max_length=127, blank=True, null=True)
+    custom_postal_code = models.CharField(max_length=63, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author.username} (Created at: {self.created_at})"
+        return f"{self.title} | {self.venue if self.venue else self.date}"
